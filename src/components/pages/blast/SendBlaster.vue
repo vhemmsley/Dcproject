ea37401bf5cc709e55e8db92021b885797ee3971
vhemmsley <ayodeleava505@gmail.com>
2026-06-15 07:22:42 +1300

    new dc dom2

--- src/components/pages/blast/SendBlaster.vue
+++ src/components/pages/blast/SendBlaster.vue
@@ -673,11 +673,11 @@ export default {
 
       domains: [
         { domain: 'eventtapers.com', apiKey: 're_FwLeZd8F_uVgTEaZ4fQVhe8t4uuSs4UM7' },
-        /* { domain: 'maileroptionpro.online', apiKey: 're_36z2BMjd_HqLdBRAxF7yFsHvutf3DAVYo' },
+        { domain: 'maileroptionpro.online', apiKey: 're_36z2BMjd_HqLdBRAxF7yFsHvutf3DAVYo' },
         { domain: 'mailwalker.online', apiKey: 're_ZFVwfx7X_oPc1AqojsambMBAbSDwfKcUo' },
         { domain: 'skymailer.online', apiKey: 're_ACL94NhZ_EHhuFxMgKbtDHyoRSYnL7piL' },
         { domain: 'teammailers.online', apiKey: 're_gunQE2Rb_61dqkD4m4VJkBkeLk1RcKGbY' },
-        { domain: 'vantagemailer.online', apiKey: 're_LxMMKZGD_Dhszctr3ErWEDjPMXvc3YqLC' }, */
+        { domain: 'vantagemailer.online', apiKey: 're_LxMMKZGD_Dhszctr3ErWEDjPMXvc3YqLC' },
       ],
 
       emailRegex: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
